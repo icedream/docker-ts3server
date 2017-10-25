e4772a7a437405ec1b8acf00046030d3490422ef
Carl Kittelberger <icedream@icedream.pw>
2017-10-26 00:30:11 +0200

    Fix healthcheck script for Alpine.

--- healthcheck.sh
+++ healthcheck.sh
@@ -2,4 +2,8 @@
 
 exec 3<>/dev/tcp/localhost/10011
 
-timeout 2 head -n1 >/dev/null <&3
+if [ "$ID" = "alpine" ]; then
+	timeout -t 2 head -n1 >/dev/null <&3
+else
+	timeout 2 head -n1 >/dev/null <&3
+fi
